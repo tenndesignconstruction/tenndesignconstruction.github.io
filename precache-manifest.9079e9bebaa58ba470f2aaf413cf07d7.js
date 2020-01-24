d9047495d9ae5aa53257767350b200c21da30eae
Todd Trivisonno <todd.trivisonno@gmail.com>
2020-01-24 10:45:43 -0500

    Updates

---
+++ precache-manifest.9079e9bebaa58ba470f2aaf413cf07d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "977de9a82148b731489d1f53caac2335",
+    "url": "/index.html"
+  },
+  {
+    "revision": "961d094a7d36a2a95425",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "ebbc577c89cbd5006c8e",
+    "url": "/static/css/main.c2c4ceb5.chunk.css"
+  },
+  {
+    "revision": "961d094a7d36a2a95425",
+    "url": "/static/js/2.77066798.chunk.js"
+  },
+  {
+    "revision": "c46b886929519117fb678111b38da6dd",
+    "url": "/static/js/2.77066798.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ebbc577c89cbd5006c8e",
+    "url": "/static/js/main.e5c9b46a.chunk.js"
+  },
+  {
+    "revision": "ddec184abff59942098d",
+    "url": "/static/js/runtime-main.ee359326.js"
+  }
+]);
